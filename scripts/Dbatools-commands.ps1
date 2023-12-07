cdda5d798f110fbfa92780a2264f04aa25b09cf5
Diana Carneiro <diana.sl.carneiro@gmail.com>
2023-12-07 12:08:42 +0000

    Update Dbatools-commands.ps1

--- scripts/Dbatools-commands.ps1
+++ scripts/Dbatools-commands.ps1
@@ -1,1 +1,14 @@
+# Finding command names that match "Connection" in the dbatools module
+Get-Command *Connection* -Module dbatools
 
+# Finding command descriptions and examples that match "connection" in the dbatools module
+Find-DbaCommand -Tag Connection
+
+# Once you’re finished installing, you may find yourself in need of documentation and help.
+# PowerShell makes getting help incredibly easy: use Get-Help, as shown in the next listing.
+
+# Example: Get help for a specific cmdlet, for example, Test-DbaConnection
+Get-Help Test-DbaConnection
+
+# Example: Get help online for a specific module
+Get-Help Test-DbaConnection -Online
