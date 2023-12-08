afc6e730f65abc6a0c9dc9091758ccfbb78b88c1
Diana Carneiro <diana.sl.carneiro@gmail.com>
2023-12-08 00:53:09 +0000

    Update WriteToSqlServer.ps1

--- scripts/WriteToSqlServer.ps1
+++ scripts/WriteToSqlServer.ps1
@@ -1,1 +1,7 @@
+#Get specific properties from the SQL Server instance and export them to a CSV file
+Get-DbaDatabase -SqlInstance SqlTest01 | Select Name, Size, LastFullBackup | Export-Csv -Path \\path\Databaseinfo.csv -NoTypeInformation
+
+# Read the contents of a file and return a string of objects
+Get-Content \\path\DatabaseInfo.csv
+
 
